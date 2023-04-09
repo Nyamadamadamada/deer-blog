2d2627d5d8bb2ada78c48c4faad84bfadfce293c
Nyamadamadamada <n.yamadamadamada@gmail.com>
2023-04-09 16:34:12 +0900

    ビルドが落ちるのを修正

--- src/components/block-title.tsx
+++ src/components/block-title.tsx
@@ -1,4 +1,5 @@
 import { Caveat } from 'next/font/google';
+import Image from 'next/image';
 import LatinWord from '@/components/latin-word';
 
 type Props = {
@@ -13,7 +14,9 @@ const caveat = Caveat({
 const BlockTitle = ({ title }: Props) => {
   return (
     <div className=''>
-      <img src={'/assets/common/underline.png'} alt={''} className='mt-[10%] h-[2.6vw]' />
+      <div className='mt-[10%] h-[2.6vw]'>
+        <Image src={'/assets/common/underline.png'} alt={''} width='100' height='200' />
+      </div>
       <div className='mt-[-20%] text-[2.6vw]'>
         <LatinWord word={title} />
       </div>
--- src/types/mds.ts
+++ src/types/mds.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/named
 import { MDXRemoteSerializeResult } from 'next-mdx-remote';
 
 export type Frontmatter = {
